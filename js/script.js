6c53406cb33c4600a38581b74d69be523f3477e6
Thiago Allisson <allissonribeiro02@gmail.com>
2021-12-03 21:16:17 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  const [form] = document.getElementsByTagName('form');
+
+  /*form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+    fetch('https://formsubmit.co/dc60e06f4f86ab0694df4936f86524cb', {
+      method: 'POST',
+      body: ev.target
+    })
+    .then(res => console.log(res));
+  });*/
+};
